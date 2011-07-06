e190f3b094dfc50ec46a7077f71aa35f17242dfa
suhas93 <suhas93@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-06 14:12:35 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tweet-random/tags/1.1@405464 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tr-options.php
@@ -0,0 +1,3 @@
+<div class="wrap">
+<h2><a href="http://tweetrandom.com/wordpress?feed=<?php echo str_replace('http://','', get_bloginfo( rss_url )); ?>">Submit your feed to TweetRandom.com </a> </h2>
+</div>
