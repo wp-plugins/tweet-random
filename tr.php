621d19869a297a2d6e1b7376f5b7f0162d17dfa0
suhas93 <suhas93@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-06 14:17:58 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tweet-random/trunk@405472 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tr.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: Tweet Random
+Version: 1.1
+Description: Creates an RSS feed for TweetRandom.com. 
+Plugin URI: http://tweetrandom.com/
+Author: Suhas Sharma
+Author URI: http://www.suhastech.com/
+*/
+
+function tr_add_options_page(  ) {
+  if ( function_exists('add_options_page') ) {
+    add_options_page( 'Manage Tweetrandom', 'TweetRandom', 'manage_options', __FILE__, 'tr_options_page' );
+  }
+  }
+  
+function tr_options_page(  ) {
+  include_once('tr-options.php');
+}
+
+function tr_add_feed(  ) {
+  global $wp_rewrite;
+  add_feed('tweetrandom', 'makeiprss');
+  add_action('generate_rewrite_rules', 'tr_rewrite_rules');
+  $wp_rewrite->flush_rules();
+}
+
+function tr_rewrite_rules( $wp_rewrite ) {
+  $new_rules = array(
+    'feed/(.+)' => 'index.php?feed='.$wp_rewrite->preg_index(1)
+  );
+  $wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
+}
+
+function makeiprss(  ) {
+  include_once('tr-template.php');
+}
+
+
+add_action('admin_menu', 'tr_add_options_page');
+add_action('init', 'tr_add_feed');
+?>
